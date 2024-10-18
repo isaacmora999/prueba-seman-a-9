c4951161af09d141b5dd5d6fc8fd35241a4fdd0b
Manuel Alejandro Jirón Martínez <165457662+ManuelJiron@users.noreply.github.com>
2024-10-18 08:39:03 -0600

    Add files via upload

---
+++ EJERCICIO2 SEMANA9/EJERCICIO2 SEMANA9/CalcularVentas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO2_SEMANA9
+{
+    internal class CalcularVentas
+    {
+        /*Crea funciones para ingresar las ventas, calcular el total vendido y
+            encontrar el día con la venta más alta.
+            - Utilizar funciones con parámetros.
+            - Implementar ciclos.*/
+
+       public static double totalVendido(double[] ventas)
+        {
+            double suma = 0;
+
+            foreach (double venta in ventas)
+            {
+                suma += venta;
+            }
+
+            return suma;
+        }
+        
+        public static void imprimirtotalVendido(double ventasTotales)
+        {
+            Console.WriteLine($"El total de ventas es {ventasTotales}");
+        }
+
+        public static int EncontrarDiaConMayorVenta(double[] ventas)
+        {
+            int diaMayorVenta = 1;
+            double mayorVenta = ventas[0];
+            for (int i = 1; i < ventas.Length; i++)
+            {
+                if (ventas[i] > mayorVenta)
+                {
+                    mayorVenta = ventas[i];
+                    diaMayorVenta = i + 1; // +1 para ajustar al índice del día
+                }
+            }
+            return diaMayorVenta;
+        }
+    }
+}
---
+++ EJERCICIO2 SEMANA9/EJERCICIO2 SEMANA9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO2_SEMANA9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Implementa un programa que use un arreglo estático para almacenar las ventas diarias
+            de un negocio. Crea funciones para ingresar las ventas, calcular el total vendido y
+            encontrar el día con la venta más alta.
+            - Utilizar funciones con parámetros.
+            - Implementar ciclos.*/
+
+            double[] ventas = new double [7];
+            double ventasTotales = 0;
+
+            Console.WriteLine("Es un programa que almacena las ventas diarias, calcula el total vendido y encuentra el dia con la venta mas alta");
+             Console.WriteLine("Ingresa las ventas por dia de la semana");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Venta del dia {i+1}: ");
+                 ventas[i] = double.Parse(Console.ReadLine());
+            }
+
+            ventasTotales = CalcularVentas.totalVendido(ventas);
+            CalcularVentas.imprimirtotalVendido (ventasTotales);
+
+            int diaMayorVenta = CalcularVentas.EncontrarDiaConMayorVenta(ventas);
+            Console.WriteLine("Día con Mayor Venta: " + diaMayorVenta);
+
+            Console.ReadKey();
+        }
+    }
+}
