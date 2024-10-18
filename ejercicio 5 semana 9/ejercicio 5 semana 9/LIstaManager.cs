f1793a596b8dd255a93cf6ba1eaa0e3cbcdba558
isaacmora999 <165749027+isaacmora999@users.noreply.github.com>
2024-10-18 08:39:32 -0600

    Add files via upload

---
+++ ejercicio 5 semana 9/ejercicio 5 semana 9/LIstaManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5_semana_9
+{
+    internal class LIstaManager
+    {
+        public static void MostrarLista(List<int> lista)
+        {
+            foreach (int num in lista)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Función para invertir la lista manualmente
+        public static List<int> InvertirLista(List<int> lista)
+        {
+            List<int> invertida = new List<int>();
+            int n = lista.Count;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                invertida.Add(lista[i]);
+            }
+            return invertida;
+        }
+
+        // Función para contar los números impares en la lista invertida
+        public static void ContarImpares(List<int> lista)
+        {
+            int contadorImpares = 0;
+            Console.WriteLine("\nNúmeros impares en la lista invertida:");
+            foreach (int num in lista)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.Write(num + " ");
+                    contadorImpares++;
+                }
+            }
+            Console.WriteLine($"\nTotal de números impares: {contadorImpares}");
+        }
+    }
+}
+
---
+++ ejercicio 5 semana 9/ejercicio 5 semana 9/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5_semana_9
+{
+    /*Escribe el programa que tenga una función que reciba un arreglo de enteros y lo
+invierta (el primer elemento se convierte en el último, el segundo en el penúltimo,
+etc.). Muestra el arreglo original y el invertido.
+- No puedes utilizar métodos ya definidos en el lenguaje.
+- Implementa una función que determine en el arreglo invertido, cuántos valores
+impares existen y los imprima*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar la longitud del arreglo
+            Console.Write("Ingresa la cantidad de elementos en el arreglo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Crear la lista y llenarla con los valores ingresados por el usuario
+            List<int> lista = new List<int>();
+            Console.WriteLine("Ingresa los valores de la lista:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Elemento {i + 1}: ");
+                lista.Add(int.Parse(Console.ReadLine()));
+            }
+
+            // Mostrar la lista original
+            Console.WriteLine("\nLista original:");
+            LIstaManager.MostrarLista(lista);
+
+            // Invertir la lista manualmente
+            List<int> listaInvertida = LIstaManager.InvertirLista(lista);
+
+            // Mostrar la lista invertida
+            Console.WriteLine("\nLista invertida:");
+            LIstaManager.MostrarLista(listaInvertida);
+
+            // Contar e imprimir los números impares en la lista invertida
+          
+           LIstaManager.ContarImpares(listaInvertida);
+      
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
